e0b6d2dbc23eca0563ab3bea28cf3f61a7dc0cec
Song Han <songhan@stanford.edu>
2015-04-14 23:26:00 -0700

    added plotting and preto curve; fixed dropout rate to 0.5 for all; setup all solver from 0.0 to 1.9

--- 1_scripts/plot.py
+++ 1_scripts/plot.py
@@ -6,52 +6,82 @@ import sys
 
 caffe_root = os.environ["CAFFE_ROOT"]
 
-if len(sys.argv)==2: 
+if len(sys.argv) == 2:
 	fileName = sys.argv[1]
-	data = open(fileName,"r")
-	x=[]
-	y1=[]
-	y5=[]
-	for line in data:		
+	data = open(fileName, "r")
+	x = []
+	y1 = []
+	y2 = []
+	y3 = []
+	for line in data:
 		x.append(float(line.split(',')[0]))
 		y1.append(float(line.split(',')[1]))
-		y5.append(float(line.split(',')[2]))
-		ss=np.array([20]*len(x))
+		y2.append(float(line.split(',')[2]))
+		try:
+			y3.append(float(line.split(',')[3]))
+		except:
+			pass
+		ss = np.array([40] * len(x))
 
-	ax = subplot(111)
-	scatter(x, y1,  s=ss)
-	scatter(x, y5,  s=ss)	
-	
-	title ("test accuracy")	
 
-	xmajorLocator   = MultipleLocator(0.1) #
-	xmajorFormatter = FormatStrFormatter('%5.1f') 
-	xminorLocator   = MultipleLocator(0.01) 
-	  	 
-	ymajorLocator   = MultipleLocator(0.05) 
-	ymajorFormatter = FormatStrFormatter('%1.4f') 
-	yminorLocator   = MultipleLocator(0.01) 
-	  
-	   
-	  	   	  
-	
-	ax.xaxis.set_major_locator(xmajorLocator)  
-	ax.xaxis.set_major_formatter(xmajorFormatter)  
-	  
-	ax.yaxis.set_major_locator(ymajorLocator)  
-	ax.yaxis.set_major_formatter(ymajorFormatter)  
-	  
-	
-	ax.xaxis.set_minor_locator(xminorLocator)  
-	ax.yaxis.set_minor_locator(yminorLocator)  
-	  
-	ax.xaxis.grid(True, which='major') 
-	ax.yaxis.grid(True, which='minor') 
+	ax1 = subplot(111)
+	ax2 = ax1.twinx()
+	ax1.scatter(x, y1, s=ss, color='g')
+	ax2.scatter(x, y2, s=ss, color='m')
+	ax2.scatter(x, y3, s=ss, color='r')
+
+	ax1.bar(x, y1, width=0.1, color='g', label='parameter count')
+# 	ax1.plot(x, y1, lw=2, color='g', label='parameter count')
+	ax2.plot(x, y2, lw=2, color='m', label='top-1 accuracy')
+	ax2.plot(x, y3, lw=2, color='r', label='top-5 accuracy')
+
+
+
+
+	xmajorLocator = MultipleLocator(0.1)  #
+	xmajorFormatter = FormatStrFormatter('%5.1f')
+	xminorLocator = MultipleLocator(0.1)
+
+	ymajorLocator = MultipleLocator(0.1)
+	ymajorFormatter = FormatStrFormatter('%1.2f')
+	yminorLocator = MultipleLocator(0.02)
+
+	ax1.xaxis.set_major_locator(xmajorLocator)
+	ax1.xaxis.set_major_formatter(xmajorFormatter)
+
+	ax1.yaxis.set_major_locator(ymajorLocator)
+	ax1.yaxis.set_major_formatter(ymajorFormatter)
+
+
+	ax1.xaxis.set_minor_locator(xminorLocator)
+	ax1.yaxis.set_minor_locator(yminorLocator)
+
+	ax1.xaxis.grid(True, which='major')
+	ax1.yaxis.grid(True, which='minor')
 
 
 	xlim(0, 2.3)
+	ax2.set_ylim(0.31, 0.81)
+	ax1.set_ylim(0, 1)
 
+
+	font = {'family' : 'normal',
+        		'weight' : 'normal',
+          	'size'   : 18}
+
+	matplotlib.rc('font', **font)
+
+	ax1.set_xlabel('Threshold');
+	ax1.set_ylabel('Parameter count');
+	ax2.set_ylabel('Accuracy');
+	grid(b=True, which='major', color='k', linestyle='-')
+
+
+	ax1.legend(loc=3)
+	ax2.legend(loc=1)
+# 	legend([ax1, ax2])
+	title ("accuracy / param count v.s threshold")
 	show()
-	
+
 else:
     print("please pass in file name!")
--- 1_scripts/pruning_net.py
+++ 1_scripts/pruning_net.py
@@ -20,6 +20,7 @@ def analyze_param(net, layers):
     print '\n=============analyze_param start==============='
     total_nonzero = 0
     total_allparam = 0
+    percentage_list = []
     for i, layer in enumerate(layers):
         i += 1
         W = net.params[layer][0].data
@@ -32,38 +33,45 @@ def analyze_param(net, layers):
         print 'W(%d) mean = %f, std = %f' % (i, np.mean(W.flatten()), np.std(W.flatten()))
         non_zero = (np.count_nonzero(W.flatten()) + np.count_nonzero(b.flatten()))
         all_param = (np.prod(W.shape) + np.prod(b.shape))
+        this_layer_percentage = non_zero / float(all_param)
         total_nonzero += non_zero
         total_allparam += all_param
         print 'non-zero W and b cnt = %d' % non_zero
         print 'total W and b cnt = %d' % all_param
-        print 'percentage = %f\n' % (non_zero / float(all_param))
+        print 'percentage = %f\n' % (this_layer_percentage)
+        percentage_list.append(this_layer_percentage)
     print '=====> summary:'
     print 'non-zero W and b cnt = %d' % total_nonzero
     print 'total W and b cnt = %d' % total_allparam
     print 'percentage = %f' % (total_nonzero / float(total_allparam))
     print '=============analyze_param ends ==============='
-    return total_nonzero / float(total_allparam)
+    return (total_nonzero / float(total_allparam), percentage_list)
 
 # options defined here:
 analyze_only = 0
-folder="/L1_2/"
+folder = "/L2/"
 
-prototxt = caffe_root+'/3_prototxt_solver/'+folder+'deploy.prototxt'
-caffemodel = caffe_root+'/4_model_checkpoint/0_original_dense/'+folder+'bvlc_alexnet.caffemodel'
-if folder[2]=='1':
+prototxt = caffe_root + '/3_prototxt_solver/' + folder + 'deploy.prototxt'
+caffemodel = caffe_root + '/4_model_checkpoint/0_original_dense/' + folder + 'bvlc_alexnet.caffemodel'
+if folder[2] == '1':
     layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6_new', 'fc7_new', 'fc8_new']
-    # layers_tbd = [ 'fc6_new', 'fc7_new', 'fc8_new']
-    layers_tbd = ['fc6']
-if folder[2]=='2':
+    layers_tbd = [ 'fc6_new', 'fc7_new', 'fc8_new']
+#     layers_tbd = ['fc8']
+if folder[2] == '2':
     layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8']
-    # layers_tbd = [ 'fc6', 'fc7', 'fc8']
-    layers_tbd = ['fc6']
+    layers_tbd = [ 'fc6', 'fc7', 'fc8']
+#     layers_tbd = ['fc8']
 else:
     print "error"
-suffix = '_fc6.caffemodel'
-output_prefix = caffe_root+'/4_model_checkpoint/1_before_retrain/'+folder+'layerwise_'
+
+suffix = '678half'
+# suffix = 'fc678'
+suffix_2 = 'alex_pruned_'
+# suffix_2 = 'layerwise_'
+output_prefix = caffe_root + '/4_model_checkpoint/1_before_retrain/' + folder + suffix_2
 threshold_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3]
-fout = open(caffe_root+'/2_results/'+folder+'parameter_cnt_678half.csv', 'w')
+fout = open(caffe_root + '/2_results/' + folder + 'parameter_cnt_' + suffix + '.csv', 'w')
+fout2 = open(caffe_root + '/2_results/' + folder + 'eachLayer_' + suffix + '.csv', 'w')
 
 
 numBins = 2 ^ 8
@@ -82,11 +90,18 @@ def prune(threshold):
     for i, layer in enumerate(layers_tbd):
         W = net.params[layer][0].data
         b = net.params[layer][1].data
-        #     hi = np.max(np.abs(W.flatten()))
+        # hi = np.max(np.abs(W.flatten()))
         hi = np.std(W.flatten())
-        mask = (np.abs(W) > (hi * threshold))
-        # if layer == layers_tbd[-1]:
-        #     mask = (np.abs(W) > (hi * threshold / 2))
+        # local_threshold is different for each layer:
+        if layer == layers_tbd[0]:
+            local_threshold = threshold
+        if layer == layers_tbd[1]:
+            local_threshold = threshold
+        if layer == layers_tbd[2]:
+            local_threshold = threshold / 2
+            if local_threshold > 1.3: local_threshold = 1.3
+
+        mask = (np.abs(W) > (hi * local_threshold))
         mask = np.bool_(mask)
         W = W * mask
         print 'non-zero W percentage = %0.4f ' % (np.count_nonzero(W.flatten()) / float(np.prod(W.shape)))
@@ -94,10 +109,10 @@ def prune(threshold):
         net.params[layer][0].mask[...] = mask
         print net.params[layer][0].mask.shape
 
-    total_percentage = analyze_param(net, layers)
-    output_model = output_prefix + str(threshold) + suffix
+    (total_percentage, percentage_list) = analyze_param(net, layers)
+    output_model = output_prefix + str(threshold) + '_' + suffix + ".caffemodel"
     net.save(output_model)
-    return (threshold, total_percentage)
+    return (threshold, total_percentage, percentage_list)
 
 # Parallel version for this code:
 # for threshold in threshold_list:
@@ -105,9 +120,15 @@ def prune(threshold):
 
 results = Parallel(n_jobs=num_cores)(delayed(prune)(threshold) for threshold in threshold_list)
 
-print results
-for (threshold, percentage) in results:
-    fout.write("%4.1f, %.4f, \n" % (threshold, percentage))
+for result in results:
+    print result
+for (threshold, total_percentage, percentage_list) in results:
+    fout.write("%4.1f, %.4f,\n" % (threshold, total_percentage))
+
+for (threshold, total_percentage, percentage_list) in results:
+    fout2.write("%4.1f, %.4f, %.4f, %.4f\n" % (threshold, percentage_list[-3], percentage_list[-2], percentage_list[-1]))
+
 fout.close()
+fout2.close()
 sys.exit(0)
 
