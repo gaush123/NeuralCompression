bd35ed8710a0a49233b400cf9eb41e09f8ad1d16
Ralph@ubuntu <1017172679@qq.com>
2015-10-18 20:12:44 +0800

    Huffman added

---
+++ 1_scripts/kmeans_quantize/compress_huff.py
@@ -0,0 +1,175 @@
+
+import sys
+import os
+import numpy as np
+import pickle
+
+# os.system("cd $CAFFE_ROOT")
+caffe_root = os.environ["CAFFE_ROOT"]
+os.chdir(caffe_root)
+print caffe_root
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+
+
+caffe.set_mode_cpu()
+option = 'alexnet'
+if option == 'lenet5':
+    prototxt = '3_prototxt_solver/lenet5/train_val.prototxt'
+    caffemodel = '4_model_checkpoint/lenet5/lenet5.caffemodel'
+    iters = 100
+    dir_t = '2_results/kmeans/lenet5/'
+elif option == 'alexnet':
+    prototxt = '3_prototxt_solver/L2/train_val.prototxt'
+    caffemodel = '4_model_checkpoint/alexnet/alexnet9x.caffemodel'
+    iters = 1000
+    dir_t = '2_results/kmeans/alexnet/'
+elif option == 'vgg':
+    prototxt = '3_prototxt_solver/vgg16/train_val.prototxt'
+    caffemodel = '4_model_checkpoint/vgg16/vgg16_12x.caffemodel'
+    iters = 1000
+    dir_t = '2_results/kmeans/vgg16/'
+
+
+caffemodel = caffe_root + caffemodel + '.quantize'
+net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+layers = filter(lambda x:'conv' in x or 'fc' in x or 'ip' in x, net.params.keys())
+
+codebook = pickle.load(open(caffe_root + 'codebook.pkl'))
+codes_W = pickle.load(open(caffe_root + 'codes.pkl'))
+
+def GetProbability(array):
+    m = np.max(array)
+    probs = map(lambda x:np.count_nonzero(array == x), range(m+1))
+    probs = map(lambda x:float(x) / len(array), probs)
+    return probs
+
+def HuffmaneEncode(probs):
+    num = len(probs)
+    codes = [0] * num
+    code_lengths = [0] * num
+    sub_groups = map(lambda x:[x], range(num))
+    sub_groups_prob = list(probs)
+
+    def find_min2(array):
+        min1 = 1.1
+        idx1 = 0
+        for i in range(len(array)):
+            if array[i] < min1:
+                idx2 = idx1
+                min2 = min1
+                idx1 = i
+                min1 = array[i]
+
+        return idx1, idx2
+
+    def append_codes(bit, idxs, codes, code_lengths):
+        for idx in idxs:
+            codes[i] = codes[i] * 2 + bit
+            code_lengths[i] += 1
+
+    for i in range(num - 1):
+        min_1, min_2 = find_min2(sub_groups_prob)
+
+        append_codes(0, sub_groups[min_1], codes, code_lengths)
+        append_codes(1, sub_groups[min_2], codes, code_lengths)
+
+        sub_groups.append(sub_groups[min_1] + sub_groups[min_2])
+        del sub_groups[min_1]
+        del sub_groups[min_2]
+
+        sub_groups_prob.append(sub_groups_prob[min_1] + sub_groups_prob[min_2])
+        del sub_groups_prob[min_1]
+        del sub_groups_prob[min_2]
+
+    return codes, code_lengths 
+
+def ArrayToCode(array, codes, code_lengths):
+    code_lengths = np.array(code_lengths, dtype = np.int32)
+    total_length = np.sum(code_lengths[array])
+    total_4bytes = (total_length - 1) / 32 + 1
+
+    compressed_codes= np.zeros(total_4bytes, dtype = np.uint32)
+
+    idx = 0
+    shift = 0
+    for number in array:
+        length = code_lengths[number]
+        code = codes[number]
+        while length > 0:
+            eff = min(length, 32-shift)
+            bits_to_write = code & ((1 << eff) - 1)
+            compressed_codes[idx] += bits_to_write << shift
+
+            code = code >> eff
+            length -= eff
+            idx += (shift + eff) / 32
+            shift += (shift + eff) % 32
+
+    assert idx * 32 + shift == total_length  # For debug
+
+    return compressed_codes, total_length
+
+def Decode(compressed_codes, total_length, codes, original_length):
+    array = np.array(original_length, dtype = np.uint8)
+
+    key = 0
+    length = 0
+    num = 0
+    for i in range(total_length):
+        idx = i / 32
+        shift = i % 32
+        bit = (compressed_codes[idx] >> shift) & 0x1
+        key += (2 ** length) * bit
+        length += 1
+        if key in codes:
+            array[num] = codes.index(key)
+            num += 1
+            key = 0
+            length = 0
+
+    assert num == original_length # For debug
+
+    return array
+        
+
+ind_codes = 4
+max_jump = 2 ** ind_codes
+nz_num = np.zeros(len(layers), np.uint32)
+spm = []
+ind = []
+for idx, layer in enumerate(layers):
+    print "Deal with layer:", layer
+    W = codes_W[layer].flatten()
+    spm_tmp = np.zeros(W.size, dtype = np.uint16)
+    ind_tmp = np.ones(W.size, dtype = np.uint16) * (max_jump-1)
+    loc = np.where(W!=0)[0]
+    distance_loc = np.append(loc[0], np.diff(loc)-1)  #jump 1 encode to 0
+    zeros = distance_loc/max_jump
+    idx_vec = np.cumsum(zeros+1)-1  #add the element itself. first one need -1
+    total_slot = idx_vec[-1]+1
+    nz_num[idx] = total_slot
+    spm_tmp[idx_vec] = W[loc]
+    ind_tmp[idx_vec] = distance_loc % max_jump
+
+    spm.append(spm_tmp[:total_slot])
+    ind.append(ind_tmp[:total_slot])
+
+    probs = GetProbability(spm[idx])
+    codes, code_lengths = HuffmaneEncode(probs)
+    compressed_codes, total_length = ArrayToCode(spm[idx])
+    recovered_array = Decode(compressed_codes, total_length, codes, len(spm[idx]))
+
+    assert np.sum(recovered_array - spm[idx]) == 0
+    sys.exit(0)
+
+'''
+nz_num.tofile(fout)
+for idx, layer in enumerate(layers):
+    codebook[layer].tofile(fout)
+    net.params[layer][1].data.tofile(fout)
+    spm[idx].tofile(fout)
+    ind[idx].tofile(fout)
+fout.close()
+'''
